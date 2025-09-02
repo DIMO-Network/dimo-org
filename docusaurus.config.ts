c9fb4396eb41c74a10da491384eabb9c6c45ceea
Barrett <barrettkowalsky@gmail.com>
2025-09-01 19:24:47 -0600

    content testing mdx

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,7 +136,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} DIMO Build.`,
     },
     prism: {
       theme: prismThemes.github,
